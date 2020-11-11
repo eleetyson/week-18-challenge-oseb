f390edda3bbd24909464b2fac6bff3eb09498f90
Ethan <eleetyson@gmail.com>
2020-11-11 18:38:29 -0500

    complete

--- src/People.js
+++ src/People.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 export default function Number(props) {
   return (
-    <div>
+    <>
       {props.astros.map((astro, index) => (
         <div key={index}>
           <p>Name: {astro.name}</p>
           <p>Spacecraft: {astro.craft}</p>
+          <br></br>
         </div>
       ))}
-    </div>
+    </>
   )
 
 }
